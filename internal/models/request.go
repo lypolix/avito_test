1bae2df6ea8d9a77a520a59e8683c1e0456685d0
Polina <plusik-006@mail.ru>
2025-11-14 20:29:58 +0300

    fix(request) fixed type of fields in requests

--- internal/models/request.go
+++ internal/models/request.go
@@ -1,34 +1,34 @@
 package models
 
 type CreateTeamRequest struct {
-    TeamName string       `json:"team_name"`
-    Members  []TeamMember `json:"members"`
+    TeamName string       `json:"team_name" binding:"required"`
+    Members  []TeamMember `json:"members" binding:"required"`
 }
 
 type SetUserActiveRequest struct {
-    UserID   string `json:"user_id"`
-    IsActive bool   `json:"is_active"`
+    UserID   string `json:"user_id" binding:"required"`
+    IsActive bool   `json:"is_active" binding:"required"`
 }
 
 type CreatePRRequest struct {
-    PullRequestID   string `json:"pull_request_id"`
-    PullRequestName string `json:"pull_request_name"`
-    AuthorID        string `json:"author_id"`
+    PullRequestID   string `json:"pull_request_id" binding:"required"`
+    PullRequestName string `json:"pull_request_name" binding:"required"`
+    AuthorID        string `json:"author_id" binding:"required"`
 }
 
 type MergePRRequest struct {
-    PullRequestID string `json:"pull_request_id"`
+    PullRequestID string `json:"pull_request_id" binding:"required"`
 }
 
 type ReassignReviewerRequest struct {
-    PullRequestID string `json:"pull_request_id"`
-    OldUserID     string `json:"old_user_id"`  
+    PullRequestID string `json:"pull_request_id" binding:"required"`
+    OldUserID     string `json:"old_user_id" binding:"required"` 
 }
 
 type GetUserPRsRequest struct {
-    UserID string `json:"user_id"` 
+    UserID string `json:"user_id" binding:"required"`  
 }
  
 type GetTeamRequest struct {
-    TeamName string `json:"team_name"`  
+    TeamName string `json:"team_name" binding:"required"`
 }
